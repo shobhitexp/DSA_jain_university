06827fd9d941821b79ad060e31a7e9e41ac1935a
SHOBHIT RAJ <115642349+shobhitexp@users.noreply.github.com>
2023-04-20 00:07:26 +0530

    update an array

---
+++ update.c
@@ -0,0 +1,17 @@
+int a[10];
+int k,n,item, i,j;
+clrscr();
+cout«<"Enter the size of the array: "«&"\n"; 
+cin>>n;
+cout<<"enter the elements of array"<<"\n";
+for (i=0;i<n;i++) cinali];
+cout<<"Enter the value to replace: "<<"\n"; 
+cin›>>item;
+cout<<"In enter the loaction to update"<<"\n"; 
+cins>>k;
+a [k-1]=item;
+cout«<"array after update: \n";
+for (i=0;i<n;i++)
+{ 
+  cout<<a[i]<<"\n";
+}
