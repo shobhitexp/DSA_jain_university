fa10633b3e470537081f1bb8df2ae2c13471e599
SHOBHIT RAJ <115642349+shobhitexp@users.noreply.github.com>
2023-04-19 23:54:32 +0530

    Deletion Operation in c

---
+++ Deletion Operation.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int arr[]={10,20,30,50,60};
+  int k=3,n=5;
+  int i,j;
+  count<<"The array elements:\n";
+  for (i = 0; i<n; i++){
+    cout<<arr[i]<<" ";
+  }
+  j=k;
+  while(j<n){
+    arr[j-1]=arr[j];
+    j=j+1;
+  }
+  n=n-1;
+  cout<<"the array after deletation:\n";
+  for (i=0; i<n; i++){
+    cout<<arr[i]<<" ";
+  }
+  return 0;
+}
